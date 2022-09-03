7f97373d4149f0b407383e5b07c71c5aee23de1d
Daria Bekhtina <dahasolo@gmail.com>
2022-09-03 11:27:48 +0300

    REQUEST_LIMIT_PER_MINUTE in settings

--- auth_flask_app/core/rate_limit.py
+++ auth_flask_app/core/rate_limit.py
@@ -2,10 +2,9 @@ from datetime import datetime
 from functools import wraps
 from http import HTTPStatus
 
+from core.config import settings
 from db.db import redis_db
 from flask import abort, jsonify, make_response, request
-
-REQUEST_LIMIT_PER_MINUTE = 20
 
 
 def rate_limit(func):
@@ -18,7 +17,7 @@ def rate_limit(func):
         pipe.expire(key, 59)
         result = pipe.execute()
         request_number = result[0]
-        if request_number > REQUEST_LIMIT_PER_MINUTE:
+        if request_number > settings.REQUEST_LIMIT_PER_MINUTE:
             abort(make_response(jsonify(message="too many requests"), HTTPStatus.TOO_MANY_REQUESTS))
         ret = func(*args, **kwargs)
         return ret
